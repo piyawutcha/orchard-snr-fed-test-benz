9c9d1841a16e8cb3ca67400598f36c43cc4545bb
piyawutcha <piyawut.cha@gmail.com>
2025-01-26 16:07:23 +1100

    Make page responsive
    * Make page responsive

--- assets/build/tasteology-js.js
+++ assets/build/tasteology-js.js
@@ -2,21 +2,21 @@ let init=()=>{console.log("Init Tasteology page");var e=[SectionOne.render(),Sec
 let SectionOne={modal_id:"section_one_modal_id",render:()=>{var e=SectionOne;return`
       <section id="section_one" class="section section_one">
         <div class="row">
-          <div class="col-7 left">
+          <div class="order-md-12 col-lg-7 left">
             ${e.renderLeft()}
           </div>
-          <div class="col-5 right">
+          <div class="order-md-12 col-lg-5 right">
             ${e.renderRight()}
           </div>
         </div>
         ${Modal.render(e.modal_id)}
       </section>
     `},renderLeft:()=>`
-      <div class="d-flex justify-content-between gap-2">
-        <div>
+      <div class="image_container d-flex justify-content-between gap-2">
+        <div class="image_left">
           <img src="assets/images/SectionOne/Left.png" alt="Left.png" class="img-fluid" onclick="SectionOne.handleImageClick(this)"/>
         </div>
-        <div>
+        <div class="image_right">
           <div class="d-flex flex-column justify-content-between gap-2">
             <div>
               <img src="assets/images/SectionOne/RightTop.png" alt="RightTop.png" class="img-fluid" onclick="SectionOne.handleImageClick(this)"/>
@@ -50,24 +50,24 @@ let SectionOne={modal_id:"section_one_modal_id",render:()=>{var e=SectionOne;ret
         <img src="${e.src}"/>
       </div>
     `);Modal.openModal(i.modal_id,e)}};
-let Card={render:i=>`
-      <div class="card_item col-4 text-center">
-        <a href="#" target="_self" onclick="Card.handleAnchorClick(this, '${i.title}', '${i.url}')">
+let Card={render:l=>`
+      <div class="card_item col-12 col-md-6 col-xl-4 text-center">
+        <a href="#" target="_self" onclick="Card.handleAnchorClick(this, '${l.title}', '${l.url}')">
           <div class="card_img">
-            <img src="${i.img_src}" alt="${i.title}" class="img-fluid" />
+            <img src="${l.img_src}" alt="${l.title}" class="img-fluid" />
           </div>
           <div class="card_content">
             <div class="card_title">
-              <h3>${i.title}</h3>
+              <h3>${l.title}</h3>
             </div>
 
             <div class="card_description">
-              <p>${i.description}</p>
+              <p>${l.description}</p>
             </div>
           </div>
         </a>
       </div>
-    `,handleAnchorClick:(i,c,l)=>{console.log("<a> tag clicked on "+c),console.log(i),console.log(`This will direct to 'Test the colours: ${c}' page by window.location.href = '${l}'`)}};
+    `,handleAnchorClick:(l,c,i)=>{console.log("<a> tag clicked on "+c),console.log(l),console.log(`This will direct to 'Test the colours: ${c}' page by window.location.href = '${i}'`)}};
 let SectionTwo={render:()=>{SectionTwo;return`
       <section id="section_two" class="section section_two">
         <div class="text-center">
@@ -90,5 +90,5 @@ let Modal={render:o=>`
         </div>
         <div id="${o}_content" class="modal_content"></div>
       </dialog>
-    `,openModal:(o,l)=>{var e=document.getElementById(o);e&&(console.log("Open modal"),Utils.inject(o+"_content",l),e.showModal())},closeModal:o=>{o=document.getElementById(o);o&&(console.log("Close modal"),o.close())}};
+    `,openModal:(o,l)=>{var e=document.getElementById(o);e&&(console.log("Open modal: "+o),Utils.inject(o+"_content",l),e.showModal())},closeModal:o=>{var l=document.getElementById(o);l&&(console.log("Close modal: "+o),l.close())}};
 let Utils={inject:(e,t)=>{e=document.getElementById(e);e&&(e.innerHTML=t)},cards:[{id:1,title:"Red",description:"Red foods remind us of berries and soft fruits, so we anticipate a sweet taste.",img_src:"assets/images/SectionTwo/Red.png",url:"/taste-the-colours/red"},{id:2,title:"Green",description:"Fresh, zingy green colours are reminiscent of unripe fruit, promising sour or acid flavours",img_src:"assets/images/SectionTwo/Green.png",url:"/taste-the-colours/green"},{id:3,title:"White",description:"White foods evoke memories of salt and salty flavours, driving the expectation of a savoury treat.",img_src:"assets/images/SectionTwo/White.png",url:"/taste-the-colours/white"}]};
--- js/SectionTwo/Card.js
+++ js/SectionTwo/Card.js
@@ -1,7 +1,7 @@
 const Card = {
   render: (card) => {
     let html = `
-      <div class="card_item col-4 text-center">
+      <div class="card_item col-12 col-md-6 col-xl-4 text-center">
         <a href="#" target="_self" onclick="Card.handleAnchorClick(this, '${card.title}', '${card.url}')">
           <div class="card_img">
             <img src="${card.img_src}" alt="${card.title}" class="img-fluid" />
