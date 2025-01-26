eff7fd4aacbc43b8736f4ac72d0843e44372d8b6
piyawutcha <piyawut.cha@gmail.com>
2025-01-26 17:49:27 +1100

    Refactor code and update readme
    * Refactor code
    * Update README.md file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const init = () => {
   console.log('Init Tasteology page');
 
+  // Get html for Section 1 and 2 and inject into div.container
   let sections = [SectionOne.render(), SectionTwo.render()].join('');
 
   Utils.inject('container', sections);
